6d1cfa609985d0d5b8fca08736ea8aabe1c6f9ce
BorisBedrenko <wadeforce@gmail.com>
2025-08-01 14:36:01 +0300

    fix(l12): fix typography

--- lesson_12/src/components/ui/Typography.jsx
+++ lesson_12/src/components/ui/Typography.jsx
@@ -5,31 +5,46 @@ function Typography({as = 'p', variant = as, className = '', ...restProps}) {
 
   return (
     <>
-      <div className={clsx(variant, className)} {...restProps} />
+      <Type className={clsx(variant, className)} {...restProps} />
       <style jsx>{`
         .p {
+          color: color-mix(in oklab, var(--foreground) 87%, var(--background));
         }
 
         .h1 {
-          font-weight: 500;
-          font-size: clamp(2.5rem, 5vi, 3rem);
-          line-height: 1.125;
+          font-weight: 800;
+          font-size: 2.25rem;
+          line-height: 1.1;
+          letter-spacing: -0.025rem;
         }
 
         .h2 {
-          font-weight: 500;
-          font-size: clamp(1.75rem, 5vi, 2rem);
-          line-height: 1.25;
+          font-weight: 600;
+          font-size: 1.875rem;
+          line-height: 1.2;
+          letter-spacing: -0.025rem;
         }
 
         .h3 {
-          font-weight: 500;
-          font-size: clamp(1.375rem, 5vi, 1.5rem);
-          line-height: 1.375;
+          font-size: 1.5rem;
+          font-weight: 600;
+          line-height: 1.3;
+          letter-spacing: -0.009rem;
+        }
+
+        .h4 {
+          font-weight: 600;
+          font-size: 1.25rem;
+          line-height: 1.4;
+          letter-spacing: -0.00625rem;
         }
 
         .code {
+          padding: 0.1875rem 0.25rem;
           font-family: monospace;
+          border-radius: 0.1875rem;
+          background-color: var(--muted);
+          color: var(--muted-foreground);
         }
       `}</style>
     </>
